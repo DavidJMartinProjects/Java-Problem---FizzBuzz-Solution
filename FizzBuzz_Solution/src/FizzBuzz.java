bf1f1b3ab12e0223571d8d541b8ec79bd13cc7f8
David Martin <davidjmartin@hotmail.com>
2016-08-01 02:49:28 +0100

    Update FizzBuzz.java

--- FizzBuzz_Solution/src/FizzBuzz.java
+++ FizzBuzz_Solution/src/FizzBuzz.java
@@ -1,12 +1,12 @@
 
 /* 
  ******************************************************************************
- *							FizzBuzz Java Problem 
+ *			FizzBuzz : Java Solution 
  ******************************************************************************
  *
- * Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” 
- * instead of the number and for the multiples of five print “Buzz”.
- * For numbers which are multiples of both three and five print “FizzBuzz”. 
+ * Write a program that prints the numbers from 1 to 100. But for multiples of three print â€œFizzâ€ 
+ * instead of the number and for the multiples of five print â€œBuzzâ€.
+ * For numbers which are multiples of both three and five print â€œFizzBuzzâ€. 
  * 
 */
 
