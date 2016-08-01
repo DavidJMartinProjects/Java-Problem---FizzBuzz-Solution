b7bbe023cbfcd73db95939c4ef80208362d70d8d
DJ <davidjmartin@hotmail.com>
2016-08-01 02:42:56 +0100

    ver 0.1 - added comments

--- FizzBuzz_Solution/src/FizzBuzz.java
+++ FizzBuzz_Solution/src/FizzBuzz.java
@@ -1,8 +1,8 @@
 
 /* 
- *************************************************************
- *					FizzBuzz Java Problem 
- *************************************************************
+ ******************************************************************************
+ *							FizzBuzz Java Problem 
+ ******************************************************************************
  *
  * Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” 
  * instead of the number and for the multiples of five print “Buzz”.
